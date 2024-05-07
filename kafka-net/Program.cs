65d90e280dfa7f59e70e9df9347eece8737e5783
nandorsilva <fernandos007@gmail.com>
2024-05-07 15:27:47 -0300

    commit

--- kafka-net/Program.cs
+++ kafka-net/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwaggerUI();
 }
 
 
